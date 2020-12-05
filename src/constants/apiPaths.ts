905bbbe309d060b3d69c28f786341737e86008e0
Shohrukh Davlatov <shohrukh@inspectorio.com>
2020-12-05 23:06:28 +0500

    init: get last updates from origin (cart)

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,8 @@ const API_PATHS = {
   product: 'https://wdkwmyi86g.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://11zu0rnv5e.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://wdkwmyi86g.execute-api.eu-west-1.amazonaws.com/dev'
+  bff: 'https://wdkwmyi86g.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
 export default API_PATHS;
