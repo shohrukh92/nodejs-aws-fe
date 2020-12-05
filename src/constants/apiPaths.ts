1efdb1cab1030b23ac8d4aa1637da9a8b595da04
Shohrukh Davlatov <shohrukh@inspectorio.com>
2020-12-06 01:15:24 +0500

    feat: set cart api url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://11zu0rnv5e.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://wdkwmyi86g.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://shohrukh92-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
