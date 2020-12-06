6bed35b9a22b9251a6b034968f972941e2f54892
Shohrukh Davlatov <shohrukh@inspectorio.com>
2020-12-06 17:51:41 +0500

    feat: add proxy to pass mixed content issue

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,8 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://11zu0rnv5e.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://wdkwmyi86g.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'http://shohrukh92-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
+  cart: 'https://tc4j0zofac.execute-api.eu-west-1.amazonaws.com/dev/api',
+  // cart: 'http://shohrukh92-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
